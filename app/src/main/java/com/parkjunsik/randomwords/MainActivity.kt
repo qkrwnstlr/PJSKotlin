13c4252528c6bae176e381f845b1aaa51ee92980
qkrwnstlr <akfldkskgorn@gmail.com>
2022-03-26 01:02:03 +0900

    2022.3.26 custom dictionary for myself (Database implementation complete)

--- app/src/main/java/com/parkjunsik/randomwords/MainActivity.kt
+++ app/src/main/java/com/parkjunsik/randomwords/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
@@ -11,38 +12,55 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.room.Room
 import com.parkjunsik.randomwords.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var db: WordDatabase
+    lateinit var data:List<Word>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java,
-            "wordsDB"
-        ).build()
-        val dbDao = db.DatabaseDao()
-        val words:List<Word> = dbDao.loadAllWords()
+        db = WordDatabase.getInstance(applicationContext)!!
+        refreshDb()
 
         with(binding){
             homeTitle.setOnClickListener {
-                Toast.makeText(this@MainActivity,"$words",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity,"$data",Toast.LENGTH_SHORT).show()
             }
             Exam.setOnClickListener {
                 val dialog = ExamDialog(this@MainActivity)
-                dialog.startDialog(words)
+                refreshDb()
+                dialog.startDialog(data)
             }
             addWord.setOnClickListener {
                 val dialog = AddDialog(this@MainActivity)
                 dialog.startDialog()
                 dialog.setOnClickedListener(object:AddDialog.ButtonClickListener{
                     override fun onClicked(newWord: String,newMeaning: String){
-                        dbDao.insert(Word(words.size,newWord,newMeaning,))
+                        insert(Word(newWord,newMeaning,Calendar.getInstance().get(Calendar.DAY_OF_MONTH)))
                     }
                 })
             }
+        }
+    }
+    private fun refreshDb(){
+        CoroutineScope(Dispatchers.Main).launch {
+            data = CoroutineScope(Dispatchers.IO).async {
+                db.DatabaseDao().loadAllWords()
+            }.await()
+        }
+    }
+    private fun insert(word: Word){
+        CoroutineScope(Dispatchers.Main).launch {
+            CoroutineScope(Dispatchers.IO).async {
+                db.DatabaseDao().insert(word)
+            }.await()
         }
     }
 }
@@ -57,12 +75,16 @@ class ExamDialog(context: Context){
         val qWord = dialog.findViewById<TextView>(R.id.questionWord)
         val qMeaning = dialog.findViewById<EditText>(R.id.questionMeaning)
         val buttonCheck = dialog.findViewById<Button>(R.id.Button_Check)
-        val test = dictionary.shuffled().toList()
+        val test = dictionary.shuffled()
         var idx = 0
         qWord.text = test[idx].word
         buttonCheck.setOnClickListener {
-            if(dictionary[idx].meaning == qMeaning.text.toString())
+            if(idx + 1 >= test.size) Log.d("clear","clear")
+            else if(test[idx].meaning == qMeaning.text.toString()) {
                 qWord.text = test[++idx].word
+                qMeaning.text = null
+            }
+            else Log.d("false","false")
         }
         dialog.show()
     }
--- app/src/main/java/com/parkjunsik/randomwords/database.kt
+++ app/src/main/java/com/parkjunsik/randomwords/database.kt
@@ -1,14 +1,16 @@
 package com.parkjunsik.randomwords
 
+import android.content.Context
 import androidx.room.*
 
 @Entity(tableName="tb_database")
 data class Word(
-    @PrimaryKey(autoGenerate = true) val id:Int,
     @ColumnInfo(name="word") var word:String,
     @ColumnInfo(name="meaning") var meaning:String,
     @ColumnInfo(name="date") var date:Int
-)
+){
+    @PrimaryKey(autoGenerate = true)var id:Int=0
+}
 
 @Dao
 interface DatabaseDao{
@@ -16,13 +18,31 @@ interface DatabaseDao{
     fun loadAllWords():List<Word>
     @Query("SELECT * FROM tb_database WHERE date == :today")
     fun loadTodayWords(today:Int):List<Word>
+
+    @Query("Delete From tb_database Where word == :word")
+    fun deleteWord(word: String)
+
     @Insert
-    fun insert(vararg words:Word)
-    @Delete
-    fun delete(word:Word)
+    fun insert(word:Word)
 }
 
 @Database(entities = [Word::class], version = 1)
-abstract class AppDatabase : RoomDatabase(){
+abstract class WordDatabase : RoomDatabase(){
     abstract fun DatabaseDao():DatabaseDao
+    companion object{
+        private var instance: WordDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context):WordDatabase?{
+            if(instance==null)
+                synchronized(WordDatabase::class){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        WordDatabase::class.java,
+                        "word-database"
+                    ).build()
+                }
+            return instance
+        }
+    }
 }
